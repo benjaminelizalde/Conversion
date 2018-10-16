96519bd611e7bff810fb52b011e19d5c9abe7796
benjaminelizalde <42652971+benjaminelizalde@users.noreply.github.com>
2018-10-16 11:08:33 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def render_main():
     return render_template('home.html')
+
+@app.route("/page1")
+def render_page1():
+    return render_template('page1.html')
+
+@app.route("/page2")
+def render_page2():
+    return render_template('page2.html')
